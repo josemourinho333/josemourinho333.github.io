97f82b032b4c7d028d5cd3250dce7029f14c6a43
josemourinho333 <yoo.phil92@gmail.com>
2022-09-06 22:29:34 -0700

    Add Intro comp and SCSS setup

---
+++ src/components/Intro/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section className="intro-container">
+      <h3 className="name">Phil Yoo</h3>
+      <p className="position">Full Stack Developer</p>
+    </section>
+  )
+}
+
+export default Intro;
