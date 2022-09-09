8b8eda47b8ab10e6a406c9174cea3fda59bc1ca9
josemourinho333 <yoo.phil92@gmail.com>
2022-09-08 19:00:50 -0700

    Update on scss

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -2,17 +2,17 @@ import React, {useState} from 'react';
 
 const Featured = () => {
   // set this to true if latest is still in progress
-  const [inProgress, setInProgress] = useState(false);
+  // const [inProgress, setInProgress] = useState(false);
 
-  const inProgressBadge = inProgress
-    ? <span className='featured-status'> - In progress </span>
-    : <span className='featured-status'> - Finished </span>
+  // const inProgressBadge = inProgress
+  //   ? <span className='featured-status'> - In progress </span>
+  //   : <span className='featured-status'> - Finished </span>
 
   return (
     <section className="featured-container">
       <h5 className="featured-header">
         Latest
-        {inProgressBadge}
+        {/* {inProgressBadge} */}
       </h5>
       <article className="featured-item">
         <img src="https://github.com/josemourinho333/josemourinho333.github.io/blob/section-featured/docs/shesa10-card.png?raw=true" alt="featured-card" className="featured-img" />
