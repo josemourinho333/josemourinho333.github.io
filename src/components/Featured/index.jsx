fa1f5c4e52a4f9d429da2faa1a5a9eb3ce6e7315
josemourinho333 <yoo.phil92@gmail.com>
2022-09-08 19:53:15 -0700

    Add card effects

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -8,13 +8,31 @@ const Featured = () => {
   //   ? <span className='featured-status'> - In progress </span>
   //   : <span className='featured-status'> - Finished </span>
 
+  const [show, setShow] = useState(false);
+
   return (
     <section className="featured-container">
       <h5 className="featured-header">
         Latest
         {/* {inProgressBadge} */}
       </h5>
-      <article className="featured-item">
+      <article className="featured-item"
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+      >
+        {
+          !show 
+          ? <></>
+          : <div className="featured-content">
+              <h5 className="content-title">'She's a 10 but ...' Generator</h5>
+              <p className="content-stack">Ruby on Rails, ReactJS, REST</p>
+              <p className="content-desc">Full stack "she's a 10 but ..." random generator. <br/>Is it trendy? Yes. Is it useless? Completely. <br/>Is it hilarious though? Maybe.</p>
+              <div className="content-links">
+                <button className="content-site"><a href="#" target="_blank" rel="noopener noreferrer">Website</a></button>
+                <button className="content-github"><a href="#" target="_blank" rel="noopener noreferrer">Github</a></button>
+              </div>
+            </div>
+        }
         <img src="https://github.com/josemourinho333/josemourinho333.github.io/blob/section-featured/docs/shesa10-card.png?raw=true" alt="featured-card" className="featured-img" />
       </article>
     </section>
