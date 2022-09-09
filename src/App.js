cfbe2869b20d773a90e0a081488a9ba187d35cc4
josemourinho333 <yoo.phil92@gmail.com>
2022-09-09 12:27:55 -0700

    Add about and current

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./scss/styles.js";
 import Intro from './components/Intro';
-import Featured from "./components/Featured/index.jsx";
+import Current from "./components/Current.jsx";
+import Featured from "./components/Featured/index";
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Intro />
+      <Current />
+      <About/>
       <Featured />
     </div>
   );
