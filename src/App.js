71922d60c347c42c733a1a946f818c7e1a9f6f49
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-20 18:32:55 -0800

    Add img of project

--- src/App.js
+++ src/App.js
@@ -29,6 +29,16 @@ function App() {
           github='https://github.com/josemourinho333/scraps'
           website=''
         />
+        <Featured 
+          current={false}
+          header={true}
+          img='https://github.com/josemourinho333/josemourinho333.github.io/blob/master/docs/scraper-card.png?raw=true'
+          name='Web Scraper'
+          stack='TypeScript, ReactJS, NodeJS, Express, Postgres, Crontab, Cheerio, NodeMailer, EJS, Tailwind, DaisyUI, Webpack'
+          desc={'Scraping Craigslist for Macbooks to resell. Cronjob set up to scrape every 30 mins and email me any good deals.'}
+          github='https://github.com/josemourinho333/scraps'
+          website=''
+        />
         {/* <Featured 
           header={false}
           img='https://github.com/josemourinho333/josemourinho333.github.io/blob/section-featured/docs/shesa10-card.png?raw=true'
