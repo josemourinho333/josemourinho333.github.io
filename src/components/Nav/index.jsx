aaa603e244cbc55503bb361043bd68ac7e985418
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-26 12:35:59 -0800

    Update navbar

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
-import { HiOutlineChevronRight } from 'react-icons/hi';
-import NavModal from '../Modal';
 import { Link } from 'react-router-dom';
 
 const Nav = () => {
+
   return (
-    <>
-      <div className="navbar bg-base-100/30 shadow-sm sticky top-0 z-50 backdrop-blur-lg justify-center h-[13vh] lg:h-[5vh]">
-        <div className="navbar-start hidden lg:flex">
-          <ul className="menu menu-horizontal p-0">
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/projects">Projects</Link></li>
-            {/* <li><Link to="/skills">Skills</Link></li> */}
+    <div className="navbar bg-neutral text-neutral-content p-0">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-1 py-2 px-7 shadow bg-neutral text-neutral-content w-screen h-[60vh] inset-x-0">
+            <li className="border-b my-2"><Link to="/about" className="text-lg">About</Link></li>
+            <li className="border-b my-2"><Link to="/projects" className="text-lg">Projects</Link></li>
+            <li className="border-b my-2"><Link to="/skills" className="text-lg">Skills</Link></li>
+            <li className="border-b my-2"><Link to="/contact" className="text-lg">Contact</Link></li>
           </ul>
         </div>
-
-        <div className="navbar-center flex flex-col">
-          <div className="avatar placeholder lg:hidden">
-            <div className="bg-gradient-to-t from-gray-500 to-gray-300 text-neutral-content rounded-full w-10">
-              <span className="text-xl font-bold">P</span>
-            </div>
-          </div> 
-          <a href="/" className="normal-case text-sm lg:text-xl font-semibold mt-0.5">Phil Yoo</a>
-          <label htmlFor="nav-modal" className="flex flex-row items-center justify-center normal-case text-xs cursor-pointer font-normal lg:hidden lg:text-sm">
-            Full Stack Developer <HiOutlineChevronRight className="text-gray-700 pt-0.5"/>
-          </label>
-        </div>
-        
-        <div className="navbar-end hidden lg:flex">
-          <a href="https://resume.creddle.io/resume/6k8iwdwj8sr" target="_blank" rel="noopener noreferrer" className="btn">Resume</a>
-        </div>
+        <a href="/" className="btn btn-ghost normal-case text-lg hidden lg:flex">Phil Yoo</a>
       </div>
-      <NavModal />
-    </>
+      <div className="navbar-center flex lg:hidden">
+        <a href="/" className="btn btn-ghost normal-case text-lg">Phil Yoo</a>
+      </div>
+
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0 text-sm">
+          <li><a>About</a></li>
+          <li><a>Projects</a></li>
+          <li><a>Skills</a></li>
+          <li><a>Contact</a></li>
+        </ul>
+      </div>
+      <div className="navbar-end p-[1rem]">
+        <a href="https://resume.creddle.io/resume/6k8iwdwj8sr" target="_blank" rel="noreferrer" className="btn btn-xs border-neutral-content md:btn-sm">Resume</a>
+      </div>
+    </div>
   )
 }
 
