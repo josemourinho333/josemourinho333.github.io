43cebbf62695d0fbdf81fde6e1cf719a7c8eeb79
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-27 20:38:33 -0800

    finish footer

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
        
         </Routes>
       </div>
-      {/* <Footer /> */}
+      <Footer />
     </>
   );
 }
--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -4,7 +4,7 @@ const WorkItem = ({year, months, position, company}) => {
   return (
     <div className="flex w-full gap-5 my-5">
       <div className="basis-1/4 flex flex-col items-end">
-        <div className="text-gray-500 text-xs text-center md:text-lg md:text-left">{year}</div>
+        <div className="text-gray-500 text-xs text-center md:text-base md:text-left">{year}</div>
         <div className="text-gray-500 text-[0.5rem] md:text-sm">{months}</div>
       </div>
       <div className="basis-3/4 flex flex-col items-start">
