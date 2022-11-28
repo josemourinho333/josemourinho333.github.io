00a55b74f8c1fda55d943dffbc3c9a5e09b51772
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-28 10:32:06 -0800

    Now consumign context api data and mapping over them

--- src/components/HomeProjectList.jsx
+++ src/components/HomeProjectList.jsx
@@ -1,16 +1,47 @@
 import React from 'react';
 import HomeProject from './HomeProject';
+import { useProjectData } from '../ProjectContext';
 
 const HomeProjectList = () => {
+  const data = useProjectData();
+  const projectList = data.slice(1, 5);
+ 
+  const projects = projectList.map((project, index) => {
+    const evens = index % 2 === 0;
+    if (!evens) {
+      return (
+        <HomeProject
+          key={index}
+          bg="bg-neutral"
+          img={project.images[0] ? project.images[0] : ''}
+          name={project.name}
+          oneLiner={project.oneLiner}
+          repo={project.repo}
+          demo={project.demo ? project.demo : ''}
+        />
+      )
+    } else {
+      return (
+        <HomeProject
+          key={index}
+          bg="bg-white"
+          img={project.images[0] ? project.images[0] : ''}
+          name={project.name}
+          oneLiner={project.oneLiner}
+          repo={project.repo}
+          demo={project.demo ? project.demo : ''}
+        />
+      )
+    }  
+  });
+  
   return (
     <div className="bg-[#fbfbfd] flex flex-col text-neutral gap-y-2 p-2">
       <div className="h-[90vh] md:h-[60vh] lg:h-[70vh] bg-inherit flex flex-col md:flex-row gap-2">
-        <HomeProject bg='bg-white' img="https://raw.githubusercontent.com/josemourinho333/scraps/master/docs/scraper-ui.PNG" name="Web Scraper" oneLiner="Be notified when someone lists a MacBook for sale below median price." repo="https://github.com/josemourinho333/scraps" demo=""/>
-        <HomeProject bg='bg-neutral'/>
+        {projects.slice(0, 2)}
       </div>
       <div className="h-[90vh] md:h-[60vh] lg:h-[70vh] bg-inherit flex flex-col md:flex-row gap-2">
-        <HomeProject bg='bg-neutral'/>
-        <HomeProject bg='bg-white'/>
+        {projects.slice(2, 4).reverse()}
       </div>
     </div>
   )
