f1dc0c7f847c1f4fcef2d0730f5d757b15b2686f
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-29 11:32:36 -0800

    Add contact layout

---
+++ src/components/Message.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className="mockup-phone shadow-lg">
+      <div className="camera"></div> 
+      <div className="display">
+        <div className="artboard artboard-demo phone-1">Hi.</div>
+      </div>
+    </div>
+  )
+}
+
+export default Message;
