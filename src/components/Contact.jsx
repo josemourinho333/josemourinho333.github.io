1217a8c32be9049b7a97b54001972b3466175492
philyoo <philyoo@philyoos-MacBook-Pro.local>
2022-11-29 14:12:48 -0800

    Finish contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,45 +1,73 @@
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import Message from "./Message";
+import { MdEmail, MdPhoneIphone } from 'react-icons/md';
+
 import { FiMail } from 'react-icons/fi';
 import { RiCloseLine } from 'react-icons/ri';
-import emailjs from '@emailjs/browser';
+// const Contact = () => {
+//   const [open, setOpen] = useState(false);
+//   const form = useRef();
 
+//   const sendEmail = (e) => {
+//     e.preventDefault();
+//     emailjs.sendForm(`${process.env.REACT_APP_SERVICE_ID}`, `${process.env.REACT_APP_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_PUBLIC_KEY}`)
+//       .then((result) => {
+//         form.current.value = '';
+//         setOpen(false);
+//       })
+//   };
 
-const Contact = () => {
-  const [open, setOpen] = useState(false);
-  const form = useRef();
+//   return (
+//     <> 
+//       {
+//         !open 
+//           ? <></>
+//           : 
+//           <div id="form-container">
+//             <form className='contact-form' ref={form} onSubmit={sendEmail}>
+//               <div className="contact-header">
+//                 <div className="contact-title">Send me a message </div>
+//                 <button className='close-form' onClick={() => setOpen(false)}><RiCloseLine /></button>
+//               </div>
+//               <input type="text" name='reply_to' placeholder='Email' />
+//               <input type="text" name='subject' placeholder='Subject'/>
+//               <textarea name="message" placeholder='Say hello!' />
+//               <button type='submit'>Send</button>
+//             </form>
+//           </div>
+//       }
+      
+//       <button className="contact-button" onClick={() => setOpen(true)}><FiMail /></button>
+//     </>
+//   )
+// }
 
-  const sendEmail = (e) => {
-    e.preventDefault();
-    emailjs.sendForm(`${process.env.REACT_APP_SERVICE_ID}`, `${process.env.REACT_APP_TEMPLATE_ID}`, form.current, `${process.env.REACT_APP_PUBLIC_KEY}`)
-      .then((result) => {
-        form.current.value = '';
-        setOpen(false);
-      })
-  };
+const Contact = () => { 
 
   return (
-    <> 
-      {
-        !open 
-          ? <></>
-          : 
-          <div id="form-container">
-            <form className='contact-form' ref={form} onSubmit={sendEmail}>
-              <div className="contact-header">
-                <div className="contact-title">Send me a message </div>
-                <button className='close-form' onClick={() => setOpen(false)}><RiCloseLine /></button>
-              </div>
-              <input type="text" name='reply_to' placeholder='Email' />
-              <input type="text" name='subject' placeholder='Subject'/>
-              <textarea name="message" placeholder='Say hello!' />
-              <button type='submit'>Send</button>
-            </form>
-          </div>
-      }
-      
-      <button className="contact-button" onClick={() => setOpen(true)}><FiMail /></button>
-    </>
+  <div className="bg-neutral-content flex flex-col justify-center p-2 gap-5 md:py-20 md:px-5 lg:flex-row">
+    <div className="basis-1/3 flex flex-col text-neutral font-black text-6xl justify-center my-10 lg:my-0 items-center lg:items-end">
+      <h2>Chat</h2>
+      <h3 className="text-gray-500 inline-block">with me</h3>
+      <p className="font-semibold text-3xl mt-3">Send me a message.</p>
+    </div>
+    <Message />
+    <div className="basis-1/3 flex flex-col text-neutral font-bold text-xl md:text-2xl justify-center my-10 lg:my-0 px-10 lg:px-0 items-center lg:items-start">
+    <div className="avatar items-center my-1">
+      <div className="w-5 rounded-full ring ring-info ring-offset-base-100 ring-offset-2">
+          <MdEmail className="w-5 h-5"/>
+        </div>
+        <a href="mailto:yoo.phil92@gmail.com" className="pl-2">yoo.phil92@gmail.com</a>
+      </div>
+      <div className="avatar items-center my-1">
+        <div className="w-5 rounded-full ring ring-success ring-offset-base-100 ring-offset-2">
+          <MdPhoneIphone className="w-5 h-5"/>
+        </div>
+        <a href="tel:+16049964504" className="pl-2">604-996-4504</a>
+      </div>
+    </div>
+  </div>
   )
-}
+};
 
 export default Contact;
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,11 +1,95 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { IoArrowUpOutline } from 'react-icons/io5';
+import MessageBubble from './MessageBubble';
+import emailjs from '@emailjs/browser';
+import moment from 'moment/moment';
 
 const Message = () => {
+  const [messages, setMessages] = useState([
+    {preset: true, msg: {name: 'phil', notes: "Hey, I'm currently based in the Fraser Valley but open to relocation"}},
+    {preset: true, msg: {name: 'phil', notes: "Do you need a website? or maybe you're looking for a developer to add to your team?"}},
+    {preset: true, msg: {name: 'phil', notes: "If so, I would love to hear from you! Send me a message here and I will get back to you ASAP. Don't forget to include your email!!"}},
+  ]);
+
+  const [templateParams, setTemplateParams] = useState({
+    preset: false,
+    msg: {
+      name: 'from portfolio',
+      notes: ''
+    },
+  });
+
+  const handleInputChange = (e) => {
+    const notes = e.target.value;
+    setTemplateParams(prev => ({
+      prev,
+      msg: {
+        ...prev.msg,
+        notes,
+      },
+    }));
+  };
+
+  const sendHandler = () => {
+    emailjs.send(`${process.env.REACT_APP_SERVICE_ID}`, `${process.env.REACT_APP_TEMPLATE_ID}`, templateParams.msg, `${process.env.REACT_APP_PUBLIC_KEY}`)
+      .then((result) => {
+        console.log('success', result.status, result.text);
+        setMessages(prev => [...prev, templateParams]);
+        setTemplateParams({
+          preset: false,
+          msg: {
+            name: 'from portfolio',
+            notes: '',
+          }
+        });
+      })
+      .catch((err) => console.log('err', err));
+  }; 
+
+  const mappedMsgs = messages.map((msg, index) => {
+    return (
+      <MessageBubble
+        key={index+768979463746}
+        message={msg.msg.notes}
+        preset={msg.preset}
+      />
+    )
+  });
+
   return (
     <div className="mockup-phone shadow-lg">
       <div className="camera"></div> 
       <div className="display">
-        <div className="artboard artboard-demo phone-1">Hi.</div>
+        <div className="artboard artboard-demo phone-1 bg-neutral overflow-y-auto justify-start w-full">
+          <div className="bg-zinc-700/80 backdrop-blur-sm text-neutral-content w-full sticky top-0 self-center min-h-[15%] flex flex-col items-center justify-end pb-1">
+            <div className="avatar placeholder">
+              <div className="bg-zinc-600 text-neutral-content rounded-full w-8">
+                <span>P</span>
+              </div>
+            </div> 
+            <h2 className="text-xs inline-block">Phil</h2>
+          </div>
+          <div className="flex-shrink-1 overflow-y-auto bg-neutral w-full h-full flex flex-col justify-end p-2">
+            <div className="text-gray-500 text-xs text-center mb-2">Today {moment().format('LT')}</div>
+            {mappedMsgs}
+          </div>
+          <div className="w-full bg-neutral/70 backdrop-blur-sm sticky bottom-0 flex justify-center pb-5 pt-1">
+            <div className="form-control w-full">
+              <div className="input-group items-center relative">
+                <input 
+                  type="text" 
+                  placeholder="Send me a msg and include your email" 
+                  className="w-full h-8 !rounded-full px-3 text-neutral-content border border-zinc-600 focus:outline-none bg-inherit" 
+                  value={templateParams.msg.notes}
+                  onChange={(e) => handleInputChange(e)}
+                />
+                <button className="" onClick={sendHandler}>
+                  <IoArrowUpOutline className="w-6 h-6 bg-inherit text-white rounded-full bg-info absolute z-50 top-1 right-1" />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
