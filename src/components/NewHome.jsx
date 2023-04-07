3be332b30705b59d461b5fddc1cffb5aa37c21a2
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-04-07 11:47:35 -0700

    Update font size and remove space

--- src/components/NewHome.jsx
+++ src/components/NewHome.jsx
@@ -8,10 +8,9 @@ import Animation from './Animation.jsx';
 const NewHome = () => {
   return (
     <div
-      className="h-screen text-black flex flex-col bg-white"
+      className="h-screen text-black flex flex-col bg-amber-200"
     >
       <div className="basis-[50%] flex flex-col px-6 md:px-40 lg:px-40 xl:px-40 xxl:px-40">
-
         <Nav />
         <div className="flex items-center h-full justify-between">
           <NewIntro />
--- src/components/NewIntro.jsx
+++ src/components/NewIntro.jsx
@@ -4,7 +4,7 @@ const NewIntro = () => {
   return (
     <div className="flex-grow flex flex-col text-lg basis-1/2 gap-4 justify-center items-start">
       <div>
-        <span className="uppercase font-black text-2xl">Currently</span> <br />building things at <span className="italic">TSB Communications</span> with awesome people.
+        <span className="uppercase font-black text-3xl">Currently</span> <br />building things at <span className="italic">TSB Communications</span> with awesome people.
       </div>
       <div>
         I'm a software developer based in the lower mainland.<br />
